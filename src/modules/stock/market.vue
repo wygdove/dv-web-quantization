f9098346400d32c77a3b93a8c3ec84c6a7460cc4
weiyg <weiyg@asiainfo.com>
2022-03-12 15:02:27 +0800

    fix warning

--- src/modules/stock/market.vue
+++ src/modules/stock/market.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <el-collapse @change="handleChange" v-model="activeName">
-      <el-collapse-item  v-for="(item,index) in collapseData" :name="item.code">
+      <el-collapse-item  v-for="(item,index) in collapseData" :name="item.code" :key="index">
         <template slot="title">
           <div class="ml-10 miw-6p  coll-title-15">{{item.name}}</div>
           <div class="ml-10 miw-10p">收盘点位：<span>{{item.cp}}</span></div>
@@ -65,8 +65,8 @@
               data[i].pb.y_10.weightedAvg.latestValPosPer=(data[i].pb.y_10.weightedAvg.latestValPos*100).toFixed(2);
               tempdata[data[i].code]=data[i];
             }
-            for(var i in that.indiceList) {
-              that.collapseData.push(tempdata[that.indiceList[i]]);
+            for(const indiceItem of that.indiceList) {
+              that.collapseData.push(tempdata[indiceItem]);
             }
           }
         });
